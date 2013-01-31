d9433020935755582dfda95c7ca7091481374ae6
Nathan Heaps <Nathan@nsh-desktop.(none)>
2013-01-30 19:40:24 -0500

    added venuelist page

---
+++ reserve.nathanheaps.com/include/venuelist.php
@@ -0,0 +1,20 @@
+<?php
+$tpl = array(
+	'PAGE_NAME' => "venuelist",
+	'PAGE_TITLE' => "On-campus Venues",
+	'PAGE_HEADER' => "On-campus Venues",
+	'CUSTOM_CSS' => array(
+		"/css/pages/venuelist.css"
+		),
+	'CUSTOM_JS' => array()
+);
+
+
+include('../static/html/header.htm');
+
+include('../static/html/venues.htm');
+
+include('../static/html/footer.htm');
+
+
+?>
