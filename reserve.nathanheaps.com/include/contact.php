fbd8654cda3f136168b2bb69482182641d813682
Nathan Heaps <Nathan@nsh-desktop.(none)>
2013-01-30 19:01:41 -0500

    Lets start again

---
+++ reserve.nathanheaps.com/include/contact.php
@@ -0,0 +1,18 @@
+<?php
+$tpl = array(
+	'PAGE_NAME' => "contact",
+	'PAGE_TITLE' => "Contact Us",
+	'PAGE_HEADER' => "Contact Us",
+	'CUSTOM_CSS' => array(),
+	'CUSTOM_JS' => array()
+);
+
+
+include('../static/html/header.htm');
+
+include('../static/html/contact.htm');
+
+include('../static/html/footer.htm');
+
+
+?>
---
+++ reserve.nathanheaps.com/include/index.php
@@ -0,0 +1,20 @@
+<?php
+$tpl = array(
+	'PAGE_NAME' => "home",
+	'PAGE_TITLE' => "Campus Reservations",
+	'PAGE_HEADER' => "Campus Reservations",
+	'CUSTOM_CSS' => array(
+		"/css/pages/index.css"
+		),
+	'CUSTOM_JS' => array()
+);
+
+
+include('../static/html/header.htm');
+
+include('../static/html/index.htm');
+
+include('../static/html/footer.htm');
+
+
+?>
